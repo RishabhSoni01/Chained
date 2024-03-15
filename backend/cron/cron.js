ecb761d84bb7088e74dc3f42389f4b9123d3ceb0
RishabhSoni01 <20U03004@iiitbhopal.ac.in>
2024-03-15 21:05:15 +0530

    added

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "";
+const URL = "https://chained-one.vercel.app/";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
